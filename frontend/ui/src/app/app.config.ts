e57e1cb6c7e08ec9bf47a547a9b92ebc73b47fc7
Tom Symonds <tsymonds@google.com>
2025-01-13 16:28:23 +0000

    Add Angular Material design.
    Install Material design with simple styling and custom theme.
    
    Change-Id: I09e78a09c2816dc00c0841f4d3a36ecb5efc78f5

--- frontend/ui/src/app/app.config.ts
+++ frontend/ui/src/app/app.config.ts
@@ -18,6 +18,7 @@ import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {routes} from './app.routes';
 
 /**
@@ -30,5 +31,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
+    provideAnimationsAsync(),
   ],
 };
