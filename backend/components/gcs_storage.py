50c41dafb5653d8717abd650fa92e416af34935f
Martina Cocco <martinacocco@google.com>
2025-01-23 11:40:23 +0000

    Added component to handle Cloud storage. Methods check if file exists in GCS, retrieve the signed path, read and write to GCS
    Change-Id: I4a78fcc61f6411f4934b0e8b41e3125c64d979fb

---
+++ backend/components/gcs_storage.py
@@ -0,0 +1,105 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Functions to handle storage operations."""
+import datetime
+from google.cloud import storage
+
+
+def check_file_exists(
+    file_name: str, bucket_name: str, storage_client: storage.Client
+) -> None:
+  """Checks if a file exists at the given bucket.
+
+  Args:
+    file_name: The name of the file to check.
+    bucket_name: The name of the bucket containing the file.
+    storage_client: The Google Cloud Storage client.
+
+  Returns:
+    True if the file exists, False otherwise.
+
+  Raises:
+    FileNotFoundError: If the file does not exist.
+  """
+  bucket = storage_client.bucket(bucket_name)
+  exists = storage.Blob(bucket=bucket, name=file_name).exists(storage_client)
+  if not exists:
+    raise FileNotFoundError(
+        f'File not found: {file_name} at bucket {bucket_name}'
+    )
+  return exists
+
+
+def get_signed_url_from_gcs(
+    bucket_name: str, file_name: str, storage_client: storage.Client
+) -> str:
+  """Gets the signed URL to a file in Google Cloud Storage.
+
+  Args:
+    bucket_name: The name of the bucket containing the file.
+    file_name: The name of the file.
+    storage_client: The Google Cloud Storage client.
+
+  Returns:
+    The signed URL to the file.
+  """
+  bucket = storage_client.bucket(bucket_name)
+  blob = bucket.blob(file_name)
+  file_path = blob.generate_signed_url(
+      datetime.timedelta(hours=1), method='GET'
+  )
+  return file_path
+
+
+def read_from_gcs(
+    bucket_name: str, file_name: str, storage_client: storage.Client
+) -> bytes:
+  """Reads a file from Google Cloud Storage.
+
+  Args:
+    bucket_name: The name of the bucket containing the file.
+    file_name: The name of the file.
+    storage_client: The Google Cloud Storage client.
+
+  Returns:
+    The content of the file.
+  """
+  bucket = storage_client.bucket(bucket_name)
+  blob = bucket.blob(file_name)
+  with blob.open('r') as f:
+    file_content = f.read()
+  return file_content
+
+
+def write_to_gcs(
+    file_content,
+    bucket_name: str,
+    file_name: str,
+    storage_client: storage.Client,
+) -> None:
+  """Writes a file to Google Cloud Storage.
+
+  Args:
+    file_content: The content of the file.
+    bucket_name: The name of the bucket containing the file.
+    file_name: The name of the file.
+    storage_client: The Google Cloud Storage client.
+  """
+  bucket = storage_client.bucket(bucket_name)
+  blob = bucket.blob(file_name)
+  # Mode can be specified as wb/rb for bytes mode.
+  # See: https://docs.python.org/3/library/io.html
+  with blob.open('w') as f:
+    f.write(file_content)
---
+++ backend/tests/gcs_storage_tests.py
@@ -0,0 +1,101 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for storage module."""
+
+import datetime
+from unittest import mock
+
+from components import gcs_storage
+from google.cloud import storage
+import pytest
+
+
+@pytest.fixture(name='mock_blob')
+def fixture_mock_blob():
+  mock_blob = mock.MagicMock(spec=storage.Blob)
+  mock_blob.generate_signed_url.return_value = (
+      'https://signed_url/test_file.mp4'
+  )
+  mock_blob.open.return_value.__enter__.return_value.read.return_value = (
+      b'This is a test file content.'
+  )
+  yield mock_blob
+
+
+@pytest.fixture(name='mock_storage_client')
+def fixture_storage_client(mock_blob):
+  mock_storage_client = mock.MagicMock(spec=storage.Client)
+  mock_bucket = mock.MagicMock(spec=storage.Bucket)
+  mock_bucket.blob.return_value = mock_blob
+  mock_storage_client.bucket.return_value = mock_bucket
+  yield mock_storage_client
+
+
+@pytest.fixture(name='mock_exists')
+def fixture_mock_storage():
+  with mock.patch.object(storage.Blob, 'exists', autospec=True) as mock_exists:
+    yield mock_exists
+
+
+@pytest.mark.parametrize('exists', [True, False])
+def test_check_file_exists_failure(mock_exists, mock_storage_client, exists):
+  mock_exists.return_value = exists
+  if not exists:
+    with pytest.raises(FileNotFoundError) as excinfo:
+      gcs_storage.check_file_exists(
+          'nonexistent_file.mp4', 'test_bucket', mock_storage_client
+      )
+    assert (
+        str(excinfo.value)
+        == 'File not found: nonexistent_file.mp4 at bucket test_bucket'
+    )
+  else:
+    assert gcs_storage.check_file_exists(
+        'existing_file.mp4', 'test_bucket', mock_storage_client
+    )
+
+
+def test_get_signed_url_from_gcs(mock_storage_client, mock_blob):
+  file_name = 'test_file.mp4'
+  expected_url = 'https://signed_url/test_file.mp4'
+
+  file_path = gcs_storage.get_signed_url_from_gcs(
+      'test_bucket', file_name, mock_storage_client
+  )
+
+  assert file_path == expected_url
+  mock_blob.generate_signed_url.assert_called_once_with(
+      datetime.timedelta(hours=1), method='GET'
+  )
+
+
+def test_read_from_gcs(mock_storage_client):
+  file_content = b'This is a test file content.'
+  returned_file_content = gcs_storage.read_from_gcs(
+      'test_bucket', 'test_file.mp4', mock_storage_client
+  )
+
+  assert returned_file_content == file_content
+
+
+def test_write_to_gcs(mock_storage_client):
+  file_content = 'This is the content to write.'
+  gcs_storage.write_to_gcs(
+      file_content, 'test_bucket', 'test_file.mp4', mock_storage_client
+  )
+  mock_storage_client.bucket().blob().open.assert_called_once_with('w')
+  mock_storage_client.bucket().blob().open.return_value.__enter__.return_value.write.assert_called_once_with(
+      file_content
+  )
