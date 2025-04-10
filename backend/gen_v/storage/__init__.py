943383987d235b30086b8ef35b61d3f289a3a1b7
Donald Seaton <dseaton@google.com>
2025-04-10 14:53:09 +0100

    Migrating GCS functions to library

---
+++ backend/gen_v/storage/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Exposes core data models for the gen_v package."""
+from gen_v.storage.gcs import download_file_locally
+from gen_v.storage.gcs import download_files
+from gen_v.storage.gcs import retrieve_all_files_from_gcs_folder
+from gen_v.storage.gcs import get_file_name_from_gcs_url
+from gen_v.storage.gcs import upload_file_to_gcs
+
+__all__ = [
+    'download_file_locally',
+    'download_files',
+    'retrieve_all_files_from_gcs_folder',
+    'get_file_name_from_gcs_url',
+    'upload_file_to_gcs',
+]
