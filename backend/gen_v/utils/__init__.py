8d1e796064d359a370913f14e054a67d5215c388
Tom Symonds <tsymonds@google.com>
2025-04-11 10:14:22 +0100

    Expose image utils via __init__.py imports.
    Change-Id: I260a02467ac2afeff570ecce1f15e80f8357ee04

--- backend/gen_v/utils/__init__.py
+++ backend/gen_v/utils/__init__.py
@@ -11,3 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Exposes core utils for the gen_v package."""
+from gen_v.utils.image import hex_to_rgb
+from gen_v.utils.image import process_and_resize_images
+from gen_v.utils.image import recolor_background_and_upload
+
+__all__ = [
+    'hex_to_rgb',
+    'process_and_resize_images',
+    'recolor_background_and_upload',
+]
